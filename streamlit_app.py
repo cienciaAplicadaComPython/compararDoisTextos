2f276497c1cf66b828df0df0c5828215e80cbc69
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-24 15:27:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ entradaDeTextoSecundaria = st.file_uploader('Anexe o segundo arquivo .txt com as
 
 botaoFoiSelecionado = st.button('Faça a mágica')
 
-if botaoFoiSelecionado:
-  'Eis os resultado:'
+if botaoFoiSelecionado and entradaDeTextoPrimaria and entradaDeTextoSecundaria):
+  'Anexe os dois arquivos!'
   
