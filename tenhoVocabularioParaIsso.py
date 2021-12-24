ef73c28d9ff229f366e0bb57cd407b7251861560
jeffersonSilvaProf <95394334+jeffersonSilvaProf@users.noreply.github.com>
2021-12-24 14:39:19 -0300

    Create tenhoVocabularioParaIsso.py

---
+++ tenhoVocabularioParaIsso.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Tenho vocabulário para isso?')
+
+'Esta aplicação ajuda você a cruzar palavras de duas fontes de entrada e retornar as palavras em comum e diferentes.'
+
+'Casos de uso:'
+'\ti) Comparar os vocabulários ensinado e de uma música para aulas de inglês;'
+'\tii) Encontrar palavras desconhecidas em um texto;'
+'\tiii) Antecipar o vocabulário necessário para um filme.'
