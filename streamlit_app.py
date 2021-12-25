dbe76ffcc5c5b6d1132b9dacceae44710cdab6e8
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-25 12:21:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ if botaoFoiSelecionado:
       dataframeSecundaria = dataframeSecundaria.stack()
       
     'Dfs dimensões:'
-    dataframePrimaria.dim
-    dataframeSecundaria.dim
+    dataframePrimaria.ndim
+    dataframeSecundaria.ndim
     
     'As palavras do segundo arquivo que estão presentes no primeiro:'
     dfComPalavrasComuns = dataframePrimaria.compare(dataframeSecundaria, align_axis = 0, keep_shape = True, keep_equal = True)
