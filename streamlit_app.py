d5575220115c8bbaaa42c849286a2c86b34e5cf9
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-25 17:40:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if botaoFoiSelecionado:
     
     if (dataframePrimaria.shape[1]  > 1) or (dataframeSecundaria.shape[1] > 1):
       'As palavras devem ser separadas em diferentes linhas. Por exemplo:'
-      df = pd.DataFrame(data = {'col1': [1, 2], 'col2': [3, 4]})
+      df = pd.DataFrame(data = {'': ['casa', 'lar', 'prédio']})
       df
     
     else:    
