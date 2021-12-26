c60dee7c9fea40499704e7d0a54cac1cd946abb9
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-26 10:50:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ if botaoFoiSelecionado:
   else:
     #dataframePrimaria = pd.read_csv(entradaDeTextoPrimaria, header = None, engine = 'python')
     #dataframeSecundaria = pd.read_csv(entradaDeTextoSecundaria, header = None, engine = 'python')
-    with open(entradaDeTextoPrimaria) as input:
-        st.text(input.read())
+    entradaDeTextoPrimaria
+    entradaDeTextoSecundaria
