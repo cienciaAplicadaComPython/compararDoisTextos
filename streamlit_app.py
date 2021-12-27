3bf48ba2173f1731da9f2d5b01f3a638ad9258fd
jeffersonSilvaProf <95394334+cienciaAplicadaComPython@users.noreply.github.com>
2021-12-27 11:21:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if botaoFoiSelecionado:
           numpySecundaria = np.delete(numpyPrimaria, posicao)
           numpySecundaria = np.append(numpyPrimaria, palavrasDeConteudo)
           break
-        posicao
+        posicao[0]
       if posicao[0] == len(numpyPrimaria) - 1:
         break
     numpyPrimaria
